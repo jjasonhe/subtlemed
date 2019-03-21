6bfbc018a7c86021fbfbbf68a8f45875fe8ae0f0
jjasonhe <jiachengjasonhe@gmail.com>
2019-03-21 01:39:09 -0500

    Add pixel spacing code
    Added dependency of Gaussian kernels on pixel spacing; if a dim has higher pixel spacing, it should have a more compressed Gaussian kernel and if a dim has lower pixel spacing, it should have a more spread out Gaussian kernel, to blur uniformly in millimeter space

--- gaussian_blur3d.py
+++ gaussian_blur3d.py
@@ -17,41 +17,56 @@ def gaussian_blur3d(input_3d: np.ndarray, meta_data: dict,
 
     :return: the blurred volume in 3D numpy array, same size as input_3d
     '''
-    # sz_x = np.round(config['sigma']/meta_data['spacing'][0])
-    # sz_y = np.round(config['sigma']/meta_data['spacing'][1])
-    # sz_z = np.round(config['sigma']/meta_data['spacing'][2])
 
-    # gauss_kern = np.zeros((sz_x, sz_y, sz_z))
+    # Calculate relative sigma values depending on pixel spacing
+    sigma_x = config['sigma']/meta_data['spacing'][0]
+    sigma_y = config['sigma']/meta_data['spacing'][1]
+    sigma_z = config['sigma']/meta_data['spacing'][2]
 
-    sigma = config['sigma']
-    kernel_half = int(np.round(3*sigma))
-
-    gauss_k = np.exp((-1/(2*sigma**2))*np.arange(-kernel_half, kernel_half+1)**2)
-    gauss_k = gauss_k / np.sum(gauss_k)
-    kernel_size = len(gauss_k)
-
-    pad_x = np.pad(input_3d, ((kernel_half,), (0,), (0,)), 'edge')
+    kernel_half_x = int(np.round(3*sigma_x))
+    kernel_half_y = int(np.round(3*sigma_y))
+    kernel_half_z = int(np.round(3*sigma_z))
+    
+    # Pad data in x
+    pad_x = np.pad(input_3d, ((kernel_half_x,), (0,), (0,)), 'edge')
     conv_x = np.zeros_like(input_3d)
+    # Create Gaussian kernel for x
+    gauss_x = np.exp((-1/(2*sigma_x**2))*np.arange(-kernel_half_x, kernel_half_x+1)**2)
+    gauss_x = gauss_x / np.sum(gauss_x)
+    kernel_size_x = len(gauss_x)
+    # Perform 1D convolutions in x
     for y in range(input_3d.shape[1]):
         for z in range(input_3d.shape[2]):
             for x in range(input_3d.shape[0]):
-                conv_x[x,y,z] = np.convolve(gauss_k, pad_x[x:x+kernel_size,y,z], 'valid')
+                conv_x[x,y,z] = np.convolve(gauss_x, pad_x[x:x+kernel_size_x,y,z], 'valid')
     # print('Done with x')
 
-    pad_y = np.pad(conv_x, ((0,), (kernel_half,), (0,)), 'edge')
+    # Pad data in y
+    pad_y = np.pad(conv_x, ((0,), (kernel_half_y,), (0,)), 'edge')
     conv_y = np.zeros_like(input_3d)
+    # Create Gaussian kernel for y
+    gauss_y = np.exp((-1/(2*sigma_y**2))*np.arange(-kernel_half_y, kernel_half_y+1)**2)
+    gauss_y = gauss_y / np.sum(gauss_y)
+    kernel_size_y = len(gauss_y)
+    # Perform 1D convolutions in y
     for x in range(input_3d.shape[0]):
         for z in range(input_3d.shape[2]):
             for y in range(input_3d.shape[1]):
-                conv_y[x,y,z] = np.convolve(gauss_k, pad_y[x,y:y+kernel_size,z], 'valid')
+                conv_y[x,y,z] = np.convolve(gauss_y, pad_y[x,y:y+kernel_size_y,z], 'valid')
     # print('Done with y')
 
-    pad_z = np.pad(conv_y, ((0,), (0,), (kernel_half,)), 'edge')
+    # Pad data in z
+    pad_z = np.pad(conv_y, ((0,), (0,), (kernel_half_z,)), 'edge')
     output_3d = np.zeros_like(input_3d)
+    # Create Gaussian kernel for z
+    gauss_z = np.exp((-1/(2*sigma_z**2))*np.arange(-kernel_half_z, kernel_half_z+1)**2)
+    gauss_z = gauss_z / np.sum(gauss_z)
+    kernel_size_z = len(gauss_z)
+    # Perform 1D convolutions in z
     for x in range(input_3d.shape[0]):
         for y in range(input_3d.shape[1]):
             for z in range(input_3d.shape[2]):
-                output_3d[x,y,z] = np.convolve(gauss_k, pad_z[x,y,z:z+kernel_size], 'valid')
+                output_3d[x,y,z] = np.convolve(gauss_z, pad_z[x,y,z:z+kernel_size_z], 'valid')
     # print('Done with z')
 
     return output_3d
